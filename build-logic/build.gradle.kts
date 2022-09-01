9459904c2fdf6ab3a022a932e8c1ddc349c40612
Goooler <wangzongler@gmail.com>
2022-09-01 10:42:53 +0800

    Use VC in build-logic

--- build-logic/build.gradle.kts
+++ build-logic/build.gradle.kts
@@ -9,12 +9,12 @@ repositories {
 // Pass '-PkotlinDev' to command line to enable kotlin-in-development version
 val kotlinVersion = if (project.hasProperty("kotlinDev")) {
     logger.warn("Enabling kotlin dev version!")
-    "1.7.20-Beta"
+    libs.versions.kotlinDev.get()
 } else {
-    "1.7.10" // Keep in sync with 'gradle/libs.versions.toml and check whether dokka-gradle-plugin has been updated as well
+    libs.versions.kotlin.get() // Keep in sync with 'gradle/libs.versions.toml and check whether dokka-gradle-plugin has been updated as well
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
-    implementation("org.jetbrains.dokka:dokka-gradle-plugin:1.7.10")
+    implementation(libs.kotlin.plugin)
+    implementation(libs.dokka)
 }
