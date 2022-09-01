7b458060a2cffe75a0b14beabb2b4c1b703c5615
Goooler <wangzongler@gmail.com>
2022-09-01 10:56:40 +0800

    Apply gradle-enterprise in build-logic

--- build-logic/settings.gradle.kts
+++ build-logic/settings.gradle.kts
@@ -1,3 +1,22 @@
+pluginManagement {
+    repositories {
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+plugins {
+    `gradle-enterprise`
+}
+
+gradleEnterprise {
+    buildScan {
+        termsOfServiceUrl = "https://gradle.com/terms-of-service"
+        termsOfServiceAgree = "yes"
+        publishAlways()
+    }
+}
+
 dependencyResolutionManagement {
     versionCatalogs {
         create("libs") {
